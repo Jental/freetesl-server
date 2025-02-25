d654733ef861150c95e86a1cb3c8e0a31a50b7b2
Nikolay Sokolov <jentalmeister@gmail.com>
2025-02-25 22:25:53 +0300

    Removed second player automation

--- match/handlers/endTurn.go
+++ match/handlers/endTurn.go
@@ -6,9 +6,43 @@ import (
 
 	"github.com/jental/freetesl-server/match"
 	"github.com/jental/freetesl-server/match/actions"
+	"github.com/jental/freetesl-server/models"
 )
 
 func EndTurn(playerID int) {
+	matchState, playerState, opponentState, err := match.GetCurrentMatchState(playerID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, card := range playerState.GetLeftLaneCards() {
+		card.IsActive = false
+	}
+	for _, card := range playerState.GetRightLaneCards() {
+		card.IsActive = false
+	}
+
+	actions.SwitchTurn(matchState)
+
+	startTurn(opponentState)
+}
+
+func startTurn(playerState *models.PlayerMatchState) {
+	playerState.SetMaxMana(playerState.GetMaxMana() + 1)
+	playerState.SetMana(playerState.GetMaxMana())
+	actions.DrawCard(playerState)
+
+	for _, card := range playerState.GetLeftLaneCards() {
+		card.IsActive = true
+	}
+	for _, card := range playerState.GetRightLaneCards() {
+		card.IsActive = true
+	}
+}
+
+// TODO: remove after bot implemented
+func EndTurnAuto(playerID int) {
 	matchState, playerState, opponentState, err := match.GetCurrentMatchState(playerID)
 	if err != nil {
 		fmt.Println(err)
