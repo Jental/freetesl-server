4f86e2e11dd04c4406b44938487a8a0552976bb4
Nikolay Sokolov <jentalmeister@gmail.com>
2025-02-25 20:01:46 +0300

    Creating match - cleanup

--- match/handlers/matchCreate.go
+++ match/handlers/matchCreate.go
@@ -3,16 +3,26 @@ package handlers
 import (
 	"encoding/json"
 	"log"
+	"maps"
+	"math/rand"
 	"net/http"
+	"slices"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 	"github.com/jental/freetesl-server/common"
+	"github.com/jental/freetesl-server/db"
+	dbModels "github.com/jental/freetesl-server/db/models"
 	"github.com/jental/freetesl-server/dtos"
 	"github.com/jental/freetesl-server/match"
 	"github.com/jental/freetesl-server/models"
 	"github.com/jental/freetesl-server/models/enums"
 	"github.com/jental/freetesl-server/services"
+	"github.com/samber/lo"
 )
+
+var rnd rand.Rand = *rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func MatchCreate(w http.ResponseWriter, req *http.Request) {
 	var playerID int = -1
@@ -77,7 +87,7 @@ func matchCreate(playerID int, opponentID int) (*uuid.UUID, error) {
 			HasValue: true,
 			Value:    &opponentState,
 		},
-		PlayerWithTurnID: playerID, // TODO: random
+		PlayerWithTurnID: selectRandomPlayer(playerID, opponentID),
 		WinnerID:         -1,
 	}
 
@@ -88,4 +98,76 @@ func matchCreate(playerID int, opponentID int) (*uuid.UUID, error) {
 	services.SetPlayerState(opponentID, enums.PlayerStateInMatch)
 
 	return &matchState.Id, nil
+}
+
+func createInitialPlayerMatchState(playerID int, conn *websocket.Conn) (models.PlayerMatchState, error) {
+	decks, err := db.GetDecks(playerID)
+	if err != nil {
+		return models.PlayerMatchState{}, err
+	}
+
+	var deckInstance []*models.CardInstance = lo.Shuffle(
+		lo.FlatMap(
+			slices.Collect(maps.Values(decks[0].Cards)),
+			func(cardWithCount dbModels.CardWithCount, _ int) []*models.CardInstance {
+				return lo.Times(cardWithCount.Count, func(_ int) *models.CardInstance {
+					return &models.CardInstance{
+						Card:           cardWithCount.Card,
+						CardInstanceID: uuid.New(),
+						Power:          cardWithCount.Card.Power,
+						Health:         cardWithCount.Card.Health,
+						Cost:           cardWithCount.Card.Cost,
+						Keywords:       cardWithCount.Card.Keywords,
+						IsActive:       false,
+					}
+				})
+			}))
+
+	var hand []*models.CardInstance = deckInstance[:3]
+	var leftDeck = deckInstance[3:]
+
+	for _, card := range hand {
+		card.IsActive = true
+	}
+
+	var playerState = models.NewPlayerMatchState(
+		playerID,
+		30,
+		5,
+		1,
+		1,
+		leftDeck,
+		hand,
+		[]*models.CardInstance{},
+		[]*models.CardInstance{},
+		[]*models.CardInstance{},
+		conn,
+	)
+
+	return playerState, nil
+}
+
+func selectRandomPlayer(player0ID int, player1ID int) int {
+	var idx = rnd.Intn(2)
+	if idx == 0 {
+		return player0ID
+	} else {
+		return player1ID
+	}
+}
+
+func updateMatchPlayerFields(matchState *models.Match) {
+	if matchState.Player0State.HasValue {
+		matchState.Player0State.Value.MatchState = matchState
+		if matchState.Player1State.HasValue {
+			matchState.Player0State.Value.OpponentState = matchState.Player1State.Value
+		}
+	}
+
+	if matchState.Player1State.HasValue {
+		matchState.Player1State.Value.MatchState = matchState
+		if matchState.Player0State.HasValue {
+			matchState.Player1State.Value.OpponentState = matchState.Player0State.Value
+		}
+	}
 }
