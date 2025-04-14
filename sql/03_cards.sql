04223882ed667c5197ae2ae569e196d28f2bd92e
Nikolay Sokolov <jentalmeister@gmail.com>
2025-04-14 23:51:42 +0300

    Fixed 03.sql

--- sql/03_cards.sql
+++ sql/03_cards.sql
@@ -2,7 +2,7 @@ INSERT INTO cards (id, name, description, power, health, cost, class_id, type_id
 VALUES (1, 'Bruma Profiteer', 'When you summon another creature, you gain 1 health.', 3, 2, 2, 1, 2),
        (2, 'Thieves Guild Recruit', 'Summon: Draw a card. If it costs 7 or more, reduce it''s cost by 1.', 1, 2, 2, 4, 2),
        (3, 'Mournhold Guardian', 'Guard', 2, 1, 1, 4, 2),
-       (4, 'Arrow in the Knee', 'Shackle a creature and deal 1 damage to it.', 0, 0, 1, 4, 1)
+       (4, 'Arrow in the Knee', 'Shackle a creature and deal 1 damage to it.', 0, 0, 1, 4, 1),
        (5, 'Knight of the Hour', 'Prophecy, Guard\nSummon: You gain 3 health.', 3, 3, 5, 1, 2);
 
 INSERT INTO card_races (card_id, race_id)
